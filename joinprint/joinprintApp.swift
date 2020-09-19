34bda5b2b174614f85f67f2f70493f6f21673786
Rachmanto Saputra <rachmantosaputra@outlook.jp>
2020-09-19 08:50:41 +0700

    Initial Commit

---
+++ joinprint/joinprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  joinprintApp.swift
+//  joinprint
+//
+//  Created by Rachmanto Saputra on 19/09/20.
+//
+
+import SwiftUI
+
+@main
+struct joinprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
